dc8e65168dd2162ee28ee2ead907f76816c449d1
scarlettdaprile <skd53@njit.edu>
2024-12-07 14:45:16 -0500

    login button, js code and edits

--- style.js
+++ style.js
@@ -1,2 +1,21 @@
 let message= "Welcome to Portfolio Star!";
 console.log(message);
+
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault(); 
+  
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+  
+    
+    const allowedUsername = "robertvella";
+    const allowedPassword = "securePassword123";
+  
+    if (username === allowedUsername && password === allowedPassword) {
+      alert("Login successful! Welcome to the portfolio website.");
+      window.location.href = "page2.html"; 
+    } else {
+      document.getElementById('error-message').innerText = "Invalid credentials. Please try again.";
+    }
+  });
+  
